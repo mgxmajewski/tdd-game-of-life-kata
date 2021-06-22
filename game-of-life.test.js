68144acb3bcc9c16913bbeb34afffcef9d00240d
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-06-22 23:06:35 +0200

    Add comments to follow given/when/then convention

--- game-of-life.test.js
+++ game-of-life.test.js
@@ -6,28 +6,36 @@ describe('Game of life', () => {
     test('should return number of rows and columns', () => {
         // Given
         const GridToTest =  new Grid(4, 8)
+        // When
         let result = GridToTest.size
+        // Then
         expect(result).toEqual('rows: 4, columns: 8');
     });
 
     test('should return array of arrays with mocked spaces', () => {
         // Given
         const GridToTest =  new Grid(4, 8)
+        // When
         let result = GridToTest.gridView
+        // Then
         expect(result).toEqual([["~", "~", "~", "~"], ["~", "~", "~", "~"], ["~", "~", "~", "~"], ["~", "~", "~", "~"], ["~", "~", "~", "~"], ["~", "~", "~", "~"], ["~", "~", "~", "~"], ["~", "~", "~", "~"]])
     });
 
     test('should return id of Space object', () => {
         // Given
         const SpaceToTest =  new Space(4, 8)
+        // When
         let result = SpaceToTest.position
+        // Then
         expect(result).toEqual(`position-4-8`)
     });
 
     test('should return alive when checked', () => {
         // Given
         const SpaceToTest =  new Space(4, 8)
+        // When
         SpaceToTest.giveLife()
+        // Then
         let result = SpaceToTest.isAlive
         expect(result).toEqual(true)
     });
