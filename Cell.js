e9011df4e6f7546c3874c2c49a262d0b7385e4eb
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-06-27 23:42:57 +0200

    Add comments to Cell

--- Cell.js
+++ Cell.js
@@ -1,4 +1,8 @@
 class Cell {
+    /**
+     * @param x - coordinate (column)
+     * @param y - coordinate (row)
+     */
     constructor(x, y) {
         this.x = x;
         this.y = y;
@@ -13,18 +17,34 @@ class Cell {
         this.alive = false;
     }
 
+    /**
+     *
+     * @return {boolean}
+     */
     get isAlive() {
         return this.alive
     }
 
+    /**
+     *
+     * @return {string}
+     */
     get position() {
         return `position-${this.x}-${this.y}`
     }
 
+    /**
+     * Return x coordinate (column)
+     * @return {number}
+     */
     get xValue() {
         return this.x
     }
 
+    /**
+     * Return y coordinate (row)
+     * @return {number}
+     */
     get yValue() {
         return this.y
     }
