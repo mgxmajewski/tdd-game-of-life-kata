5cac2c43943d8ffe44dca33a6f15d9bfd988df24
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-06-27 23:37:01 +0200

    Add comments in GameOfLife class

--- GameOfLife.js
+++ GameOfLife.js
@@ -6,12 +6,24 @@ class GameOfLife {
         this.columns = columns
         this.rows = rows
         this.grid = new Grid(columns, rows)
+        this.initiateLife = []
     }
 
+    /**
+     * Returns actual grid
+     * @return {Object} Grid
+     */
     get cellGrid(){
         return this.grid
     }
 
+    /**
+     * Revives all cells specified in provided argument.
+     * Uses Cell objects accessCell method using provided
+     * x,y coordinates (each array is one cell) to revive
+     * each cell by using giveLife method
+     * @param initialLife {Array} - array of arrays
+     */
     set initiateLife(initialLife){
         for(let cell = 0; cell < initialLife.length; cell++){
             const x = initialLife[cell][0]
@@ -21,10 +33,28 @@ class GameOfLife {
         }
     }
 
+    /**
+     * Updates Grid
+     * @param {Grid} updatedGrid
+     */
     set updatedGrid(updatedGrid){
         this.grid = updatedGrid
     }
 
+    /**
+     * Computes next state of the Grid.
+     * Initially creates blank temporary Grid (all cells dead) and according
+     * to rules of Conway's Game of Life revive cells. No cells need to be killed
+     * because they are dead by default in initiated tempGrid.
+     *
+     * Only two of four rules need need to be applied to achieve correct state:
+     *  3. Any live cell with two or three live neighbours lives on to the next generation.
+     *  4. Any dead cell with exactly three live neighbours becomes a live cell.
+     *
+     * In this method analiseNeighbourCells method of actual Grid is called which returns
+     * number required for decision making. Cells which are decided to be revived are yielded
+     * in cellsToRevive array. It is provided as argument to initiateLife setter.
+     */
     updateGrid(){
         const tempGrid = new Grid(this.columns, this.rows)
         const cellsToRevive = []
